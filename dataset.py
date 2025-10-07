a97ba5d8328cb29f4dbee75e23945a3cbbc580c2
AsSakina <79874800+AsSakina@users.noreply.github.com>
2025-10-07 10:45:49 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -70,7 +70,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 # Load the CSS file
-local_css("style.css")
+# local_css("style.css")
 
 
 def add_custom_css():
